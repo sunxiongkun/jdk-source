95c51e0060e4cb3001bbfaaf81608e45645f415e
sunxiongkun <sunxiongkun2018@163.com>
2019-01-06 18:41:32 +0800

    java source init

---
+++ java11/src/main/java/jdk.javadoc/jdk/javadoc/internal/tool/resources/version.java
@@ -0,0 +1,11 @@
+package jdk.javadoc.internal.tool.resources;
+
+public final class version extends java.util.ListResourceBundle {
+    protected final Object[][] getContents() {
+        return new Object[][] {
+            { "full", "11.0.1+13-LTS" },
+            { "jdk", "11.0.1" },
+            { "release", "11.0.1" },
+        };
+    }
+}
